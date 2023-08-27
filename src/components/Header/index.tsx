ee230cd0d3963b1b0429f59d6113ded511c0790a
Aristides Pascoal Queta <aristidesquetajr@gmail.com>
2023-08-27 14:18:08 +0100

    feat: added react-modal
    create a modal for create new transactions

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -2,12 +2,18 @@ import { Container, Content } from './styles'
 
 import logoImg from '../../assets/logo.svg'
 
-export function Header() {
+interface HeaderProps {
+  onOpenNewTransactionModal: () => void
+}
+
+export function Header({ onOpenNewTransactionModal }: HeaderProps) {
   return (
     <Container>
       <Content>
         <img src={logoImg} alt="dt modey" />
-        <button type="button">Nova transação</button>
+        <button type="button" onClick={onOpenNewTransactionModal}>
+          Nova transação
+        </button>
       </Content>
     </Container>
   )
