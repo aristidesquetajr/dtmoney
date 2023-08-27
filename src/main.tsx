5ecb15cf1cb868236138f613867013742e803a1e
Aristides Pascoal Queta <aristidesquetajr@gmail.com>
2023-08-27 12:47:44 +0100

    feat: added fake api with miragejs

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,28 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 
+import { createServer } from 'miragejs'
+
 import { App } from './App'
+
+createServer({
+  routes() {
+    this.namespace = 'api'
+
+    this.get('/transactions', () => {
+      return [
+        {
+          id: 1,
+          title: 'Desenvolvimento de site',
+          amount: 200000,
+          type: 'deposit',
+          category: 'venda',
+          createdAt: new Date(),
+        },
+      ]
+    })
+  },
+})
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
