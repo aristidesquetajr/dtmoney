e4bdb7c2859cb6a523a717851c2d58b36326570d
Aristides Pascoal Queta <aristidesquetajr@gmail.com>
2023-08-27 22:29:46 +0100

    feat: added list of transactions

--- src/components/TransactionsTable/index.tsx
+++ src/components/TransactionsTable/index.tsx
@@ -1,10 +1,23 @@
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import { api } from '../../services/api'
 import { Container } from './styles'
 
+interface Transaction {
+  id: number
+  description: string
+  amount: number
+  type: string
+  category: string
+  createdAt: string
+}
+
 export function TransactionsTable() {
+  const [transactions, setTransactions] = useState<Transaction[]>([])
+
   useEffect(() => {
-    api.get('/transactions').then((response) => console.log(response.data))
+    api.get('/transactions').then((response) => {
+      setTransactions(response.data.transactions)
+    })
   }, [])
 
   return (
@@ -19,24 +32,23 @@ export function TransactionsTable() {
           </tr>
         </thead>
         <tbody>
-          <tr>
-            <td>Desenvolvimento de site</td>
-            <td className="deposit">Kz 50.000,00</td>
-            <td>Venda</td>
-            <td>13/04/2022</td>
-          </tr>
-          <tr>
-            <td>Hamburguer</td>
-            <td className="withdraw">- Kz 2.500,00</td>
-            <td>Alimentação</td>
-            <td>10/04/2022</td>
-          </tr>
-          <tr>
-            <td>Aluguel do apartamento</td>
-            <td className="withdraw">- Kz 12.500,00</td>
-            <td>Casa</td>
-            <td>10/04/2022</td>
-          </tr>
+          {transactions.map((transaction) => (
+            <tr key={transaction.id}>
+              <td>{transaction.description}</td>
+              <td className={transaction.type}>
+                {new Intl.NumberFormat('pt-AO', {
+                  style: 'currency',
+                  currency: 'AOA',
+                }).format(transaction.amount)}
+              </td>
+              <td>{transaction.category}</td>
+              <td>
+                {new Intl.DateTimeFormat('pt-AO').format(
+                  new Date(transaction.createdAt),
+                )}
+              </td>
+            </tr>
+          ))}
         </tbody>
       </table>
     </Container>
--- src/main.tsx
+++ src/main.tsx
@@ -10,6 +10,29 @@ createServer({
     transaction: Model,
   },
 
+  seeds(server) {
+    server.db.loadData({
+      transactions: [
+        {
+          id: 1,
+          description: 'Freelancer de site',
+          amount: 200000,
+          type: 'deposit',
+          category: 'Dev',
+          createdAt: new Date('2023-07-23 15:43:00'),
+        },
+        {
+          id: 2,
+          description: 'Aluguel',
+          amount: 40000,
+          type: 'withdraw',
+          category: 'Casa',
+          createdAt: new Date('2023-08-24 09:14:40'),
+        },
+      ],
+    })
+  },
+
   routes() {
     this.namespace = 'api'
 
